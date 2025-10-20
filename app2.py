5ef516ec0cc67caaad351be79ecd882ee2ba863e
Aggelos1991 <aggelosmc@gmail.com>
2025-10-20 09:13:35 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -116,7 +116,7 @@ def match_invoices(erp_df, ven_df):
             r"^remesa",             # Spanish
             r"^pago",               # Spanish
             r"^transferencia",      # Spanish
-            r"(?i)^f[-\s]?\d{4,8}",
+            r"(?i)^#?\s*f[-\s]?\d{2,8}"
         ]
         if any(re.search(p, reason) for p in payment_patterns):
             return "IGNORE"
