01cdec4b5a08935cc35181faef21513e64619501
Aggelos1991 <aggelosmc@gmail.com>
2025-10-28 16:59:25 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -676,7 +676,7 @@ if uploaded_erp and uploaded_vendor:
                 use_container_width=True
             )
 
- except Exception as e:
-        st.error(f"Error: {e}")
-        st.info("Check that your files contain columns like: **invoice**, **debit/credit**, **date**, **reason**")
+     except Exception as e:
+            st.error(f"Error: {e}")
+            st.info("Check that your files contain columns like: **invoice**, **debit/credit**, **date**, **reason**")
 
