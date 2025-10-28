2cbd486a851f798532aba780e41884a838cf0e67
Aggelos1991 <aggelosmc@gmail.com>
2025-10-28 17:09:14 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -688,9 +688,6 @@ if uploaded_erp and uploaded_vendor:
                 use_container_width=True
             )
 
-except Exception as e:
-    st.error(f"Error: {e}")
-    st.info("Check that your files contain columns like: **invoice**, **debit/credit**, **date**, **reason**")
 
 
 # ==================== EXCEL EXPORT (Only Unified Missing Sheet) =========================
