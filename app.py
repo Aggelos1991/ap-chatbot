e560137131fd7e1e782070cd90be895c095b772e
Aggelos1991 <aggelosmc@gmail.com>
2025-10-29 13:42:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ if pay_file:
     cn_rows = []
 
     # ---- Apply CN logic only if CN file exists ----
-   if cn is not None:
+if cn is not None:
         cn_alt_col = find_col(cn, ["Alt.Document", "Alt. Document"])
         cn_val_col = find_col(cn, ["Amount"])
 
