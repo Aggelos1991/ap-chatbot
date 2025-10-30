1087aab09f422147b32a44027e9ad5af4156b34b
Aggelos1991 <aggelosmc@gmail.com>
2025-10-30 10:24:58 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,7 +9,7 @@ import pytesseract
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # ==========================================================
 # CONFIGURATION
