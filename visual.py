305faadffef52dbd65a9061c09da6cc60990b1c0
Aggelos1991 <aggelosmc@gmail.com>
2025-11-06 09:22:57 +0200

    Update visual.py

--- visual.py
+++ visual.py
@@ -4,8 +4,8 @@ import plotly.express as px
 import numpy as np
 
 st.set_page_config(page_title="Overdue Invoices", layout="wide")
-st.title("The Lord of the Debits,One invoice to rule them all, one invoice to find them,
-One invoice to bring them all, and in the ledger bind them.")
+st.title("💍 The Lord of the Debits 🧾\nOne invoice to rule them all, one invoice to find them,\nOne invoice to bring them all, and in the ledger bind them. ⚔️")
+
 
 
 # --- SESSION STATE ---
